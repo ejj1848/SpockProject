98b0ffd6a1dcd7e0a0aa775354276c624809467e
Eric.Johnson <eric.johnson@astontech.com>
2018-01-17 13:53:22 -0600

    Added Changes to WHY Class

--- src/test/java/com/aston/why.java
+++ src/test/java/com/aston/why.java
@@ -4,4 +4,20 @@ package com.aston;
  * Created by ericjohn1 on 9/21/2016.
  */
 public class why {
+
+
+    private  String  name ;
+
+    public why(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
